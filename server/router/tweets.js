2a4fc064b4498a5e102bf96a334e6e6dd6aa0473
DevelopJKong <ybaram40@gmail.com>
2022-01-05 12:27:05 +0900

    14.6 드위터에 유효성 검사 적용해 보기 완료

--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,7 +1,23 @@
 import express from "express";
 import "express-async-errors";
-import * as tweetController from '../controller/tweets';
+import {body} from "express-validator";
+import * as tweetController from "../controller/tweets";
+import { validate } from "../middleware/validator";
+
+
+// validation
+// sanitization
+// Contract Testing : Client-Server
+
 const tweetsRouter = express.Router();
+
+const validateTweet = [
+  body("text")
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage("text should be at least 3 characters"),
+  validate,
+];
 
 // GET /tweets
 // GET /tweets?username=:username // => : 은 정확히 무슨 뜻이지? // 삼항 연산자
@@ -12,12 +28,12 @@ tweetsRouter.get("/", tweetController.getTweets);
 tweetsRouter.get("/:id", tweetController.getTweet);
 
 // POST /tweets
-tweetsRouter.post("/", tweetController.createTweet);
+tweetsRouter.post("/", validateTweet, tweetController.createTweet);
 
 // PUT /tweets/:id
-tweetsRouter.put("/:id", tweetController.updateTweet);
+tweetsRouter.put("/:id",validateTweet, tweetController.updateTweet);
 
-// DELETE /tweets/:id
-tweetsRouter.delete("/:id",tweetController.deleteTweet );
+// DELETE /tweets/:id7
+tweetsRouter.delete("/:id", tweetController.deleteTweet);
 
 export default tweetsRouter;
